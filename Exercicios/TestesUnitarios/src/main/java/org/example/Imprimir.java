6e346767fb9f78586e90e406c43c8066fb2cc361
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-05-30 14:49:20 -0300

    Classe Imprimir

---
+++ Exercicios/TestesUnitarios/src/main/java/org/example/Imprimir.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Imprimir {
+
+
+    public String imprimir(double num) {
+        if (num % 3 == 0 && num % 5 == 0) {
+            return "FizzBuzz";
+        } else if (num % 3 == 0) {
+            return "Fizz";
+        } else if (num % 5 == 0) {
+            return "Buzz";
+        } else {
+            return "";
+        }
+    }
+}
